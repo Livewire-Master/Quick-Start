4780f81ba7582b0d69e134005ba1f0524991f854
RealMrHex <RealMrHex@gmail.com>
2024-02-01 19:48:31 +0330

    writing the view

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div style="text-align: center">
+    <h1>Counter Component</h1>
+    <pre>Clicked <span>0</span> times!</pre>
+    <div>
+        <button>
+            Increment
+        </button>
+
+        <button>
+            Decrement
+        </button>
+    </div>
 </div>
